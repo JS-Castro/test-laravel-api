d9477a26458607ec2bc6cba262c98c28eb55368c
João Castro <87384790+JS-Castro@users.noreply.github.com>
2024-01-15 00:55:02 +0000

    clean up some code + added game model

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,50 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Http\Client\ResponseSequence;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
 
-    public function register(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|string|min:6',
-        ]);
-
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
-        return response()->json(['user' => $user, 'message' => 'User registered successfully'], 201);
-    }
-
     public function login(Request $request)
     {
-        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
-            // successfull authentication
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             $user = User::find(Auth::user()->id);
 
-            $user_token['token'] = $user->createToken('appToken')->accessToken;
+            $token = $user->createToken('appToken')->accessToken;
 
             return response()->json([
                 'success' => true,
-                'token' => $user_token,
+                'token' => $token,
                 'user' => $user,
             ], 200);
-        } else {
-            // failure to authenticate
+        }
+
             return response()->json([
                 'success' => false,
                 'message' => 'Failed to authenticate.',
             ], 401);
-        }
     }
 
     public function logout(Request $request)
